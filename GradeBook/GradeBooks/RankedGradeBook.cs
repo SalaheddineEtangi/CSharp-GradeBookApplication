c03c9586acefe1f424d5a6c6aa81241fdef6cfad
Salah-eddine Etangi <mo.momen1438@gmail.com>
2023-03-12 13:33:26 +0100

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -21,6 +21,24 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException();
             }
 
+            List<double> averages = Students.Select(s => s.AverageGrade).ToList();
+            averages.Sort();
+
+            int numberOfStudentsGradeA_ShouldPass = numberOfStudents - (int)(numberOfStudents * 0.2);
+            int numberOfStudentsGradeB_ShouldPass = numberOfStudents - (int)(numberOfStudents * 0.4);
+            int numberOfStudentsGradeC_ShouldPass = numberOfStudents - (int)(numberOfStudents * 0.6);
+            int numberOfStudentsGradeD_ShouldPass = numberOfStudents - (int)(numberOfStudents * 0.8);
+
+            double gradeA_threshold = averages[numberOfStudentsGradeA_ShouldPass];
+            double gradeB_threshold = averages[numberOfStudentsGradeB_ShouldPass];
+            double gradeC_threshold = averages[numberOfStudentsGradeC_ShouldPass];
+            double gradeD_threshold = averages[numberOfStudentsGradeD_ShouldPass];
+
+            if(averageGrade > gradeA_threshold) return 'A';
+            if(averageGrade > gradeB_threshold) return 'B';
+            if(averageGrade > gradeC_threshold) return 'C';
+            if(averageGrade > gradeD_threshold) return 'D';
+
             return 'F';
         }
 
